73e67e5d86eb2239b04693e8ad72d52b94a6afe9
Eazyisreal <eazyisreal192@gmail.com>
2023-02-22 22:19:41 +0100

    fix: fixed deployment bugs

--- Test/Test/wsgi.py
+++ Test/Test/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for Test project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-# import sys
-# sys.path.insert(0, 'Test')
+import sys
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR, 'Test'))
 
 from django.core.wsgi import get_wsgi_application
 
